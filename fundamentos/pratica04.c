a9ea8efe6d0ad8f3a436e16eddc73b785a9cf7c9
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-04 11:18:43 -0400

    Update pratica04.c

--- fundamentos/pratica04.c
+++ fundamentos/pratica04.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main(){
-    int idade = 
-scanf("%d",idade)
+char nome[50];
+int idade;
+float altura;
+
+printf("inserir nome: ");
+scanf("%s", nome);
+
+printf ("inserir idade: ");
+scanf("%d", &idade);
+
+printf("inserir altura: ");
+scanf("%f",&altura);
+
+printf("\nola,%s você tem %d anos e mede %.2f metros\n",&nome,idade,altura);
 }
