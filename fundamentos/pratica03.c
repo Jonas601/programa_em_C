e86a82a4fc6ed77486b4c357b8d5a41c81ab5599
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-04 10:24:39 -0400

    .

---
+++ fundamentos/pratica03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int x_global_var = 10;
+ 
+int main() {
+int x_local_var = 5;
+
+
+printf("Variavel global: %d\nVariavel local: %d\n",x_global_var,x_local_var);
+
+return 0; 
+} 
