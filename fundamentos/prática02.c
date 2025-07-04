7407afbd0d591e3db67ef853d780d22c5fd6e1e1
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-04 08:23:32 -0400

    Update prática02.c

--- fundamentos/prática02.c
+++ fundamentos/prática02.c
@@ -6,7 +6,7 @@ char pet_nome[10] = "bolt";
 float pet_peso = 12.3;
 float pet_altura = 0.45;
 
-
+printf("Informações do Pet\n");
 printf(" nome: %s \n idade: %d anos \n peso: %0.2f kg \n altura:%0.2f m \n",pet_nome,pet_idade,pet_peso,pet_altura);
 
 return 0;
