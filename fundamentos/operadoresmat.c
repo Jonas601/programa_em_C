bf5b18d32fccbf24b19e71222f41d445ac2ed35e
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-08 10:12:14 -0400

    Update operadoresmat.c

--- fundamentos/operadoresmat.c
+++ fundamentos/operadoresmat.c
@@ -1,1 +1,23 @@
-j
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a;
+    int b;
+    
+    printf("digite o primeiro numero: ");
+    scanf("%d",&a);
+    
+    printf("digite o segundo numero: ");
+    scanf("%d",&b);
+    
+    if( a == b ){
+        printf("os dois são iguais :%d , %d",a,b);
+    }
+    if(a > b) {
+        printf("o primeiro valor é maior que o segundo: %d , %d", a,b);
+    }
+    if( a < b){
+        printf("o primeiro valor é menor que o segundo: %d , %d",a,b);
+    }
+    
+}
