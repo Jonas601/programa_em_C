fc5e977e088d7eca04dd277d9138345b0bcaf5c3
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-08 11:07:43 -0400

    Update operadoresmat.c

--- fundamentos/operadoresmat.c
+++ fundamentos/operadoresmat.c
@@ -20,3 +20,37 @@ int main(){//definição de variaveis
         printf("o primeiro valor é menor que o segundo: %d , %d",a,b);
     }
 }
+#include <stdio.h>
+
+int main(){//definição de variaveis
+    int a;
+    int b;
+    
+    printf("digite o primeiro valor: ");
+    scanf("%d",&a);
+    
+    printf("digite o segundo valor: ");
+    scanf("%d",&b);
+    
+    if( a == b ){//teste de igualdade
+        printf("\nos dois são iguais :%d , %d\n",a,b);
+    }
+    if(a != b){
+        printf("\no primeiro valor não é igual ao segundo %d , %d\n",a,b);
+    }
+    if(a > b) {//teste de diferença
+        printf("\no primeiro valor é maior que o segundo valor: %d , %d\n", a,b);
+    }
+    if(a < b){
+        printf("\no primeiro valor é menor que o segundo valor: %d , %d\n",a,b);
+    }
+    if(a >= b){//teste de proximidade
+        printf("\no primeiro valor é maior ou igual ao segundo valor: %d , %d\n",a,b);
+    }
+    if(a <= b){
+        printf("\no primeiro valor é menor ou igual ao segundo valor: %d , %d\n",a,b);
+    }
+    
+    return 0;
+    
+}
