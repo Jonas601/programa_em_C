aa7be0adf1d360748231da89b38b5f6a1f32830b
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-10 11:23:44 -0400

    mudanças de incremento

--- laçoderepetição.c
+++ laçoderepetição.c
@@ -11,3 +11,22 @@ int main(){
         printf("%d\n",contador);
     }// fim de laço de repetição com decremento
 }
+#include <stdio.h> //pares 
+
+int main(){
+    int contador;
+    
+    for(contador = 0 ;contador <= 20; contador +=2){
+        printf("%d\n",contador);
+    }
+} 
+
+#include <stdio.h>//contador infinito
+
+int main(){
+    int contador;
+    
+    for(contador = 1 ;contador >=0; contador ++){
+        printf("%d\n",contador);
+    }
+} 
