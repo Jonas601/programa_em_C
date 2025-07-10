7856da67d58cbaf474b6183109b1c47a1040fa0b
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-10 10:31:54 -0400

    Update encremento.c

---
+++ fundamentos/encremento.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+  int main (){
+      int passos = 0;//variaveis 
+    
+        printf("o valor de passos é %d",passos);//demonstrar valor inicial de passos
+        
+        passos+=3;
+        printf("\no valor de passos é %d",passos);//demonstrar valor incrementado com ++ ou +=
+        
+        passos--; 
+        printf("\no valor de passos é %d",passos);//demonstra valor decrementado com -- ou -=
+    
+   
+  }
