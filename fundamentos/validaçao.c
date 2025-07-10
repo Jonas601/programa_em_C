ffa4086eb04bebf986b87f3fcc51f9b46686f1d0
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-10 09:49:30 -0400

    Update validaçao.c

--- fundamentos/validaçao.c
+++ fundamentos/validaçao.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int idade;
+    int idade;//variaveis
     int ingresso;
 
-    printf("Digite sua idade: ");
+    printf("Digite sua idade: ");//campo de dados
     scanf("%d", &idade);
 
-    printf("Possui um ingresso?\nDigite 1 para sim\nDigite 0 para não: ");
+    printf("Possui um ingresso?\nDigite 1 para sim\nDigite 0 para não: "); //campo de dados 
     scanf("%d", &ingresso);
 
-    if (ingresso != 0 && ingresso != 1) {
+    if (ingresso != 0 && ingresso != 1) { //primeira condição para caso o valor seja diferente do esperado.
         printf("Digite somente 1 ou 0, por favor. Execute o programa novamente.\n");
-        scanf("%d", &ingresso);
+        scanf("%d", &ingresso);//novo campo de correção
 
     }
 
-    if (idade >= 14 && ingresso == 1) {
+    if (idade >= 14 && ingresso == 1) {//verificar as condições mínima
         printf("Pode entrar no cinema.\n");
-    } else if (idade >= 14 && ingresso == 0) {
+    } else if (idade >= 14 && ingresso == 0) {//caso apenas atenda uma das condições 
         printf("Idade mínima atingida, mas não tem ingresso.\n");
-    } else {
+    } else {// caso não atenda nenhuma das condições mínimas
         printf("Não pode entrar no cinema.\n");
     }
 
