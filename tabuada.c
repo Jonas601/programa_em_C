8055156884fabdc603ac8864fdcba889780c41a0
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-11 09:16:46 -0400

    Update tabuada.c

--- tabuada.c
+++ tabuada.c
@@ -15,14 +15,14 @@ int main(){
 #include <stdio.h>
 
 int main(){
-    int numero,x;
+    int numero,x;//declara variaveis
      
     printf("digite um valor de 1 a 10: ");
-    scanf("%d",&numero);
+    scanf("%d",&numero);// campo para o valor digitado
     
-    for(x = 1;x <= 50 ; x++){
-        int result = numero*x;
-        printf("\n%d x %d = %d",numero,x,result);
+    for(x = 1;x <= 50 ; x++){//declara valor de "x" e  até onde ir
+        int result = numero*x;//campo da operação desejada
+        printf("\n%d x %d = %d",numero,x,result);//demosntrar o resultado
     }
     
 }
