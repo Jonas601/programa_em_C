f0298cc3cd8937984de565057a2f95dcf3b79f89
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-14 09:56:03 -0400

    novas aulas

---
+++ fundamentos/forandfor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    int y;
+     
+     for(x = 1; x <= 5; x++){
+        
+        for(y= 1; y <= 5; y++){
+            printf("%dx%d=%d\n",x,y,x * y);
+        }
+        printf("\n");
+     }
+}
--- fundamentos/prova_prática1.c
+++ fundamentos/prova_prática1.c
@@ -1,11 +1,26 @@
 //jonas soares costa
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(){ 
+    bool aluno_1 = true;
+    bool aluno_0 = false;
+    int reposta;
     const float nota_media = 6;//declarando variaveis
     char nome_completo[100];
     float nota1,nota2;
+    
+    printf("você é um aluno ?\n digite 1 para sim \n digite 0 para não: ");
+    scanf("%d",&reposta);
+     
+     if(reposta == 1){
+         printf("pode prosseguir é um aluno");
+     }
+     else{
+         printf("não pode prosseguir não é um aluno");
+     }
+    
     
     printf("\ndigite seu nome completo: ");//campo de preenchimento nome completo
      fgets(nome_completo,sizeof(nome_completo),stdin);
@@ -18,8 +33,8 @@ int main(){
           
           float nota_total = ((nota1 + nota2)/2);
           
-          if(nota1 == 0 || nota2 == 0 ){//condião para recuperação de nota
-              printf("\nerr... %s você não passou com media %.1f ,mas é possivel fazer a recuperação",nome_completo,nota_total);
+          if(nota_total <= nota_media){//condião para recuperação de nota
+              printf("\nerr... %svocê não passou com media %.1f ,mas é possivel fazer a recuperação",nome_completo,nota_total);
             }
           
           if(nota_total >= nota_media ){//condição de media
