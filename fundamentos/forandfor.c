b5f9313902bfb96cd1754b22e0220031927d21b5
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-14 10:22:05 -0400

    mudanças

--- fundamentos/forandfor.c
+++ fundamentos/forandfor.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(){
+int main(){//variaveis
     int x;
     int y;
      
-     for(x = 1; x <= 5; x++){
+     for(x = 1; x <= 5; x++){// for sobre for 
         
         for(y= 1; y <= 5; y++){
-            printf("%dx%d=%d\n",x,y,x * y);
+            printf("%dx%d=%d\n",x,y,x * y);//estrutura
         }
         printf("\n");
      }
--- fundamentos/while.c
+++ fundamentos/while.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main() {//variaveis
     int numero;
     int soma = 0;
     
-    while (1) {
+    while (1) {//repetiçao deve ser 1 para não dar erro 
         printf("Digite um número: ");
         scanf("%d", &numero);
 
-        if (numero == 0) {
+        if (numero == 0) {//condição para parar 
             break;
         }
-        soma += numero;
+        soma += numero;//soma 
     }
     printf("A soma total dos números digitados é: %d\n", soma);
 
