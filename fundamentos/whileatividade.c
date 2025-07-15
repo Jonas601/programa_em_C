e670603a857c67545e3ab44d2cb76d5dd5873909
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-15 10:13:32 -0400

    Update whileatividade.c

--- fundamentos/whileatividade.c
+++ fundamentos/whileatividade.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main() {//declarando tres variaveis para não dar erro na hora do incremento
     int number,par = 0, x =0;
     
     
-    while( x < 10){
-        printf("digite o %d° número: ", x + 1);
-        scanf("%d",&number);
+    while( x < 10){//declarando quantidade de numeros a receber
+        printf("digite o %d° número: ", x + 1);//incrementar mais 1 ao x e receber varios numeros
+        scanf("%d",&number);//chamando o numero que o usuario digitou
+        getchar();//limpar memoria
         
-        if(number %2 == 0){
-            par++;
+        if(number %2 == 0){//condição que testa para poder contar um número par
+            par++;//os números par é somado e mostra a quantidade entre os 10
         }
-       x++; 
+       x++; //incremento para o número x 
     }
-       printf("quantidade de pares %d ",par);
+       printf("quantidade de pares %d ",par);//mostrar a quantidade de pares entre os 10 números digitados
     return 0;
 }
