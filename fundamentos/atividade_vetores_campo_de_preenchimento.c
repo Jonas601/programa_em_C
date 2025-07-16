9acb0f9be9d21a2256aedc1d826dee2d175c2402
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-16 11:53:09 -0400

    Update atividade_vetores_campo_de_preenchimento.c

--- fundamentos/atividade_vetores_campo_de_preenchimento.c
+++ fundamentos/atividade_vetores_campo_de_preenchimento.c
@@ -5,14 +5,14 @@ int main() {
     int valor;
 
     //para armazenar os valores no vetores com for
-    for(valor = 0; valor < 5; valor++) {
+    for(valor = 0; valor < 5; valor++) {//incremento para número de perguntas 
         printf("Digite o %dº número inteiro: ", valor + 1);
         scanf("%d", &numeros[valor]);
     }
 
     // para exibir os valores armazenados com for
     printf("\nOs números digitados foram:\n");
-    for(valor = 0; valor < 5; valor++) {
+    for(valor = 0; valor < 5; valor++) {//incremento valor ao número
         printf("\nposição %d ; valor: %d ",valor, numeros[valor]);
     }
 
