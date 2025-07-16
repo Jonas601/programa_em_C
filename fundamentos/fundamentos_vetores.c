ba15bdf7f1a5a81b76a451e8117644219b3b1e78
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-16 11:24:53 -0400

    Update fundamentos_vetores.c

--- fundamentos/fundamentos_vetores.c
+++ fundamentos/fundamentos_vetores.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char nomes_alunos[5][10]= {"0","Jonas ","Wender ","Fernanda ","Darillem "};
+    char nomes_alunos[5][10]= {"0","Jonas ","Wender ","Fernanda ","Darillem "};//CADEIA DE  variaveis
     int numeros_alunos[4] = { 1, 2 , 3 , 4 };
     float nota_aluno [5] = {0, 10.0 , 6.6 , 8.9 , 7.8 };
     int cont;
     
-    printf("nomes de alunos: ");
+    printf("nomes de alunos: ");//logica de alunos
     for(cont = 1 ; cont < 5; cont++){
     printf("\naluno %d : %s",cont, nomes_alunos[cont]);
     }
