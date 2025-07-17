6efa76c8cdb063a769e2d4c1eeb72a67367458ba
Jonas Soares <jonastrabalho31@gmail.com>
2025-07-17 10:19:40 -0400

    Update arrays_string.c

--- fundamentos/arrays_string.c
+++ fundamentos/arrays_string.c
@@ -15,7 +15,7 @@ int main() {
     char nomes[num][100];
     
     for (int cont = 0; cont < num; cont++) {
-        printf("Digite o nome %d: ", cont + 1);
+        printf("Digite o %d° nome completo: ", cont + 1);
         fgets(nomes[cont], 100, stdin);
         nomes[cont][strcspn(nomes[cont], "\n")] = '\0';
     }
@@ -23,7 +23,7 @@ int main() {
     printf("\n");
     printf("Nomes cadastrados:\n\n");
     for (int cont = 0; cont < num; cont++) {
-        printf("%s\n", nomes[cont]);
+        printf("%d°: %s\n", cont + 1, nomes[cont]);
     }
  
   return 0;
